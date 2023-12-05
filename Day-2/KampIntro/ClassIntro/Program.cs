f5f439a0236d62052b5fd9eea62f11f0e0ebce7d
Cesurbedel <150424156+Cesurbedel@users.noreply.github.com>
2023-12-05 19:17:36 +0300

    Add files via upload

---
+++ Day-2/KampIntro/ClassIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Cesur";
+            int yas = 17;
+
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C++";
+            kurs1.Egitmen = "Cesur Bedel";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Ali Enver";
+            kurs2.IzlenmeOrani = 45;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Sonya Enver";
+            kurs3.IzlenmeOrani = 76;
+
+            //Console.WriteLine(kurs1.KursAdi + " " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] {kurs1, kurs2, kurs3};
+
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen );
+            }
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}

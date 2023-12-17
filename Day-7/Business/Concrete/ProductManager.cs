efdcf73e65243e5f94e8c397302f312397332984
Cesurbedel <150424156+Cesurbedel@users.noreply.github.com>
2023-12-18 01:31:30 +0300

    Add files via upload

---
+++ Day-7/Business/Concrete/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductIService
+    {
+        IProductDal _ProductDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _ProductDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //Iskodlari
+            //Yetkisi var mi?
+            return _ProductDal.GetAll();
+
+
+        }
+    }
+}

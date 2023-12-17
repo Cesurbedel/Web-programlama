c0c4acc87bbf99a6c1a738c9780fd8e882668df8
Cesurbedel <150424156+Cesurbedel@users.noreply.github.com>
2023-12-18 01:37:26 +0300

    Add files via upload

---
+++ KampIntro/Donguler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı",
+                "Proglamlamaya başlangıç için temel kurs", "java", "Python","C++"};
+
+            for (int i = 0; i<kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu.");
+        }
+    }
+}

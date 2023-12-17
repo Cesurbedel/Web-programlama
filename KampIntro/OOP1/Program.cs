422eacd24b678ded535a2e12c97b1ad06c4a54bf
Cesurbedel <150424156+Cesurbedel@users.noreply.github.com>
2023-12-18 01:40:47 +0300

    Add files via upload

---
+++ KampIntro/OOP1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitInStock = 100;
+
+            Product product2 = new Product();
+
+            product2.Id = 2;
+            product2.CategoryId = 3;
+            product2.ProductName = "Kanepe";
+            product2.UnitPrice = 300;
+            product2.UnitInStock = 100;
+
+
+            Product product3 = new Product { Id = 2, CategoryId = 5, UnitInStock = 5, ProductName = "Kalem", UnitPrice = 35 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+        }
+    }
+}
